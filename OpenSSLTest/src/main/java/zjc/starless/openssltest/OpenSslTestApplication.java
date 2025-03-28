54c78816d2e7a38e5be1000d641c3ddd432865c5
findnamed <1482099959@qq.com>
2025-03-28 16:47:15 +0800

    添加证书文件配置，添加hello接口测试

--- OpenSSLTest/src/main/java/zjc/starless/openssltest/OpenSslTestApplication.java
+++ OpenSSLTest/src/main/java/zjc/starless/openssltest/OpenSslTestApplication.java
@@ -2,8 +2,9 @@ package zjc.starless.openssltest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class OpenSslTestApplication {
 
     public static void main(String[] args) {
