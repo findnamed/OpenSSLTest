8531a693ee50a780378f80a94359474d68c17395
findnamed <1482099959@qq.com>
2025-03-28 16:29:41 +0800

    初始化

---
+++ OpenSSLTest/src/main/java/zjc/starless/openssltest/OpenSslTestApplication.java
@@ -0,0 +1,13 @@
+package zjc.starless.openssltest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenSslTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpenSslTestApplication.class, args);
+    }
+
+}
---
+++ OpenSSLTest/src/test/java/zjc/starless/openssltest/OpenSslTestApplicationTests.java
@@ -0,0 +1,13 @@
+package zjc.starless.openssltest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenSslTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
